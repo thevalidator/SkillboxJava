5b7fbee89b20dbcd341f289f4e8c12aefb829913
the.validator <the.validator@yandex.ru>
2019-07-24 12:11:06 +0300

    module 003 task 1

--- module_003/Cat/src/Cat.java
+++ module_003/Cat/src/Cat.java
@@ -7,29 +7,35 @@ public class Cat
     private double minWeight;
     private double maxWeight;
 
+    public boolean isAlive;
+
     public Cat()
     {
         weight = 1500.0 + 3000.0 * Math.random();
         originWeight = weight;
         minWeight = 1000.0;
         maxWeight = 9000.0;
+        isAlive = true;
 
     }
 
     public void meow()
     {
         weight = weight - 1;
+        getStatus();
         System.out.println("Meow");
     }
 
     public void feed(Double amount)
     {
         weight = weight + amount;
+        getStatus();
     }
 
     public void drink(Double amount)
     {
         weight = weight + amount;
+        getStatus();
     }
 
     public Double getWeight()
@@ -40,9 +46,11 @@ public class Cat
     public String getStatus()
     {
         if(weight < minWeight) {
+            isAlive = false;
             return "Dead";
         }
         else if(weight > maxWeight) {
+            isAlive = false;
             return "Exploded";
         }
         else if(weight > originWeight) {
--- module_003/Cat/src/Loader.java
+++ module_003/Cat/src/Loader.java
@@ -6,5 +6,35 @@ public class Loader
         Cat cat = new Cat();
 
         System.out.println(cat.getStatus());
+
+        Cat cat1 = new Cat();
+        System.out.println("\n" + "cat 1:" + "\n" + "=============================");
+        System.out.println("weight: " + cat1.getWeight());
+        cat1.feed(20000.);
+        System.out.println("weight after feedng: " + cat1.getWeight());
+        System.out.println("status: " + cat1.getStatus());
+
+        Cat cat2 = new Cat();
+        System.out.println("\n" + "cat 2:" + "\n" + "=============================");
+        System.out.println("weight: " + cat2.getWeight());
+        cat2.drink(30.);
+        System.out.println("weight after drinking: " + cat2.getWeight());
+        System.out.println("status: " + cat2.getStatus());
+
+        Cat cat3 = new Cat();
+        System.out.println("\n" + "cat 3:" + "\n" + "=============================");
+        System.out.println("weight: " + cat3.getWeight());
+
+        Cat cat4 = new Cat();
+        System.out.println("\n" + "cat 4:" + "\n" + "=============================");
+        System.out.println("weight: " + cat4.getWeight());
+
+        Cat cat5 = new Cat();
+        System.out.println("\n" + "cat 5:" + "\n" + "=============================");
+        System.out.println("weight: " + cat5.getWeight());
+        while (cat5.isAlive) {
+            cat5.meow();
+        }
+        System.out.println("status: " + cat5.getStatus());
     }
 }
