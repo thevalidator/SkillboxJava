9f2f9a0023d716d24596d7230348347921a83d75
the.validator <the.validator@yandex.ru>
2019-08-07 21:59:52 +0300

    module 4 hometask 2

--- module_004/IntegerExperiments/src/Main.java
+++ module_004/IntegerExperiments/src/Main.java
@@ -7,6 +7,15 @@ public class Main
 
         int sum = sumDigits(12300456);
         System.out.println("Сумма всех цифр числа равна: " + sum);
+
+     //   byte.int.short.long.double,float
+        System.out.println("   int: " +  Integer.MAX_VALUE + " / " + Integer.MIN_VALUE);
+        System.out.println("double: " +  Double.MAX_VALUE + " / " + Double.MIN_VALUE);
+        System.out.println("  byte: " +  Byte.MAX_VALUE + " / " + Byte.MIN_VALUE);
+        System.out.println(" float: " +  Float.MAX_VALUE + " / " + Float.MIN_VALUE);
+        System.out.println("  long: " +  Long.MAX_VALUE + " / " + Long.MIN_VALUE);
+        System.out.println(" short: " +  Short.MAX_VALUE + " / " + Short.MIN_VALUE);
+
     }
 
     public static Integer sumDigits(Integer number)
