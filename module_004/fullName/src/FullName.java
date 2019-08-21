6ba4a3d772da93fe0f6a6d81e7bb763d3950f722
the.validator <the.validator@yandex.ru>
2019-08-21 13:28:03 +0300

    module 4 task 4 more corrections

--- module_004/fullName/src/FullName.java
+++ module_004/fullName/src/FullName.java
@@ -3,68 +3,60 @@ import java.util.Scanner;
 public class FullName
 {
     private static Scanner scanner = new Scanner(System.in);
-    private static String name;
     private static char spaceChar = ' ';
 
-
     public static void main(String[] args) {
+        String name = "empty empty empty";
 
+        System.out.println("First name: " + getFistName(name));
 
         System.out.print("Enter Full Name (First, Last, Middle): ");
-        askName();
+        name = askName();
 
-        while (!isCorrectName()) {
+        /*
+        while (!isCorrectName(name)) {
             System.out.print("Error, please enter the correct name: ");
-            askName();
+            name = askName();
         }
+        */
 
-        System.out.println("Last name: " + getLAstName());
-        System.out.println("First name: " + getFistName());
-        System.out.println("Middle name: " + getMiddleName());
+        System.out.println("Last name: " + getLAstName(name));
+        System.out.println("First name: " + getFistName(name));
+        System.out.println("Middle name: " + getMiddleName(name));
 
     }
 
-    public static void askName() {
-        name = (scanner.nextLine()).trim();
+    public static String askName() {
+        //return (scanner.nextLine()).trim();
+        String name = (scanner.nextLine()).trim();
+        while (!isCorrectName(name)) {
+            System.out.print("Error, please enter the correct name: ");
+            name = (scanner.nextLine()).trim();
+        }
+        return name;
     }
 
-    private static boolean isCorrectName() {
+    private static boolean isCorrectName(String name) {
         if (name.indexOf(spaceChar) == -1) {
             return false;
         }
-        if ((getLAstName().indexOf(spaceChar) != -1) || ( getLAstName().length() == 0 )) {
+        if ((getLAstName(name).indexOf(spaceChar) != -1) || ( getLAstName(name).length() == 0 )) {
             return false;
         } else {
             return true;
         }
     }
 
-    private static String getFistName() {
+    private static String getFistName(String name) {
         return (name.substring(0, name.indexOf(spaceChar)));
     }
 
-    private static String getMiddleName() {
+    private static String getMiddleName(String name) {
         return ((name.substring(name.lastIndexOf(spaceChar))).trim());
     }
 
-    private static String getLAstName() {
-        return ((name.substring((getFistName().length()), (name.length() - getMiddleName().length()))).trim());
+    private static String getLAstName(String name) {
+        return ((name.substring((getFistName(name).length()), (name.length() - getMiddleName(name).length()))).trim());
     }
-
-
-    /*
-    while (name.indexOf(spaceChar) == -1) {
-            reEnterFullName();
-        }
-        firstName = name.substring(0, name.indexOf(spaceChar));
-        middleName = (name.substring(name.lastIndexOf(spaceChar))).trim();
-        lastName = (name.substring((firstName.length()), (name.length() - middleName.length()))).trim();
-        while ( (lastName.indexOf(spaceChar) != -1) || ( lastName.length() == 0 ) ) {
-            reEnterFullName();
-            firstName = name.substring(0, name.indexOf(spaceChar));
-            middleName = (name.substring(name.lastIndexOf(spaceChar))).trim();
-            lastName = (name.substring((firstName.length()), (name.length() - middleName.length()))).trim();
-        }
-     */
 
 }
