cd72ef5bbd49a55ab011765ac963b0b8529377b8
thevalidator <the.validator@yandex.ru>
2020-04-14 17:21:58 +0300

    task 04 - corrections with Jsoup downloading, download method and getting name of file method

--- module_009/task04/src/Main.java
+++ module_009/task04/src/Main.java
@@ -1,50 +1,45 @@
 import org.jsoup.Jsoup;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
 import java.net.URL;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.nio.file.*;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        try {
-            List<String> imagesList = Jsoup.connect("https://lenta.ru").get()
-                    .select("img[class=g-picture]").eachAttr("src");
-            ArrayList<Path> directories = new ArrayList<>();
-            Path tempFolder = Paths.get("tmp");
-            Path imagesFolder = Paths.get("images");
-            Path imageAddressLinks = tempFolder.resolve("readMe.txt");
-            directories.add(tempFolder);
-            directories.add(imagesFolder);
+        String url = "https://lenta.ru";
+        Path tempFolder = Paths.get("tmp");
+        Path imagesFolder = Paths.get("images");
+        Path imageAddressLinks = tempFolder.resolve("readMe.txt");
 
-            for (Path dir : directories) {
-                if (Files.notExists(dir)) {
-                    Files.createDirectory(dir);
-                }
+        try {
+
+            List<String> imagesList = Jsoup.connect(url).maxBodySize(0).get()
+                    .select("img[class=g-picture]").eachAttr("src");
+
+            for (Path dir : Arrays.asList(tempFolder, imagesFolder)) {
+                Files.createDirectories(dir);
             }
+
             if (Files.notExists(imageAddressLinks) ) {
                 Files.createFile(imageAddressLinks);
             }
 
-            FileWriter writer = new FileWriter(String.valueOf(imageAddressLinks), false);
             imagesList.forEach(str -> {
-                try {
-                    String fileName = imagesFolder.resolve(str.substring(str.lastIndexOf("/") + 1)).toString();
+                try (FileWriter writer = new FileWriter(String.valueOf(imageAddressLinks), false)) {
+                    String fileName = imagesFolder.resolve(Paths.get(URI.create(str)
+                            .getPath()).getFileName()).toString();
                     download(str, fileName);
                     writer.write(str + "\n");
                 } catch (IOException ex) {
                     ex.printStackTrace();
                 }
             });
-            writer.close();
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -52,13 +47,12 @@ public class Main {
 
     }
 
-    private static void download(String urlStr, String file) throws IOException {
-        URL url = new URL(urlStr);
-        ReadableByteChannel rbc = Channels.newChannel(url.openStream());
-        FileOutputStream fos = new FileOutputStream(file);
-        fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-        fos.close();
-        rbc.close();
+    private static void download(String FILE_URL, String FILE_NAME) {
+        try (InputStream in = new URL(FILE_URL).openStream()) {
+            Files.copy(in, Paths.get(FILE_NAME), StandardCopyOption.REPLACE_EXISTING);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
