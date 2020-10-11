c8dcd70276624bc75a5032bb0bd3ac09298ce2ab
thevalidator <the.validator@yandex.ru>
2020-10-11 09:23:19 +0300

    module 10 task 2 (some fixes)

--- module_010/task02/src/main/java/Main.java
+++ module_010/task02/src/main/java/Main.java
@@ -11,47 +11,24 @@ public class Main {
 
     public static void main(String[] args) {
 
-/*        try {
-            setUp();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
-
-        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
-        Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
-        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
-
-        Session session = sessionFactory.openSession();
-        for (int i = 1; i < 4; i++) {
-            Course course = session.get(Course.class, i);
-            System.out.println(course.getName() + ": " + course.getPrice() + " руб.");
-        }
-        Student student = session.get(Student.class, 1);
-        System.out.println(student.getName() + " " + student.getRegistrationDate());
-
-        session.close();
-        sessionFactory.close();
-    }
-
-
-
-    static protected void setUp() throws Exception {
-        // A SessionFactory is set up once for an application!
-        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-                .configure() // configures settings from hibernate.cfg.xml
-                .build();
         try {
-            sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+            StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+            Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
+            SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
 
             Session session = sessionFactory.openSession();
-            Course course = session.get(Course.class, 1);
-            System.out.println(course.getName());
+            for (int i = 1; i < 4; i++) {
+                Course course = session.get(Course.class, i);
+                System.out.println(course.getName() + ": " + course.getPrice() + " руб.");
+            }
+            Student student = session.get(Student.class, 1);
+            System.out.println(student.getName() + " " + student.getRegistrationDate());
             session.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            sessionFactory.close();
         }
-        catch (Exception e) {
-            // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
-            // so destroy it manually.
-            StandardServiceRegistryBuilder.destroy( registry );
-        }
+
     }
 }
